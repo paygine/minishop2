c44c4fcf4cb1706c7a0e343c4948d5fbad9efb1c
IgorEkimov <igor.ekimov@paygine.net>
2023-09-15 09:20:03 +0300

    Module update

--- assets/components/minishop2/payment/paygine.php
+++ assets/components/minishop2/payment/paygine.php
@@ -4,7 +4,6 @@ require dirname(dirname(dirname(dirname(dirname(__FILE__))))) . '/index.php';
 
 $modx->getService('error','error.modError');
 
-//Logging request if in debug mode
 if ($modx->getDebug()) $modx->log(xPDO::LOG_LEVEL_DEBUG, '[miniShop2:Paygine] Payment notification request: ' . print_r($_REQUEST, true));
 
 /* @var miniShop2 $miniShop2 */
@@ -14,36 +13,41 @@ $miniShop2->loadCustomClasses('payment');
 $response = '';
 $context = '';
 $params = array();
+
 if (class_exists('Paygine')) {
-    /* @var msPaymentInterface|Paygine $handler */
-    $handler = new Paygine($modx->newObject('msOrder'));
-    if (!empty($_REQUEST['reference'])) {
-        $order = $modx->getObject('msOrder', $_REQUEST['reference']);
-        if (isset($order)) {
-            $response = $handler->receive($order, $_REQUEST);
-            $context = $order->get('context');
-            $params['msorder'] = $order->get('id');
-        } else
-            $response = $handler->paymentError('Order not found', $_REQUEST);
-    } else {
-        $modx->log(xPDO::LOG_LEVEL_ERROR, '[miniShop2:Paygine] Wrong orderId.');
-    }
+	/* @var msPaymentInterface|Paygine $handler */
+	$handler = new Paygine($modx->newObject('msOrder'));
+
+	if (!empty($_REQUEST['reference'])) {
+		$order = $modx->getObject('msOrder', $_REQUEST['reference']);
+
+		if (isset($order)) {
+			$response = $handler->receive($order, $_REQUEST);
+			$context = $order->get('context');
+			$params['msorder'] = $order->get('id');
+		} else
+			$response = $handler->paymentError('Order not found', $_REQUEST);
+	} else {
+		$modx->log(xPDO::LOG_LEVEL_ERROR, '[miniShop2:Paygine] Wrong orderId.');
+	}
 } else {
-    $modx->log(xPDO::LOG_LEVEL_ERROR, '[miniShop2:Paygine] could not load payment class "Paygine".');
+	$modx->log(xPDO::LOG_LEVEL_ERROR, '[miniShop2:Paygine] could not load payment class "Paygine".');
 }
-//echo $response;
 
 $success = $cancel = $modx->getOption('site_url');
 
 if ($id = $modx->getOption('setting_ms2_payment_paygine_success_id', null, 0)) {
-    $success = $modx->makeUrl($id, $context, $params, 'full');
+	$success = $modx->makeUrl($id, $context, $params, 'full');
 }
+
 if ($id = $modx->getOption('setting_ms2_payment_paygine_cancel_id', null, 0)) {
-    $cancel = $modx->makeUrl($id, $context, $params, 'full');
+	$cancel = $modx->makeUrl($id, $context, $params, 'full');
 }
+
 if ($response){
-    $redirect = $success;
+	$redirect = $success;
 } else {
-    $redirect = $cancel;
+	$redirect = $cancel;
 }
+
 $modx->sendRedirect($redirect);
